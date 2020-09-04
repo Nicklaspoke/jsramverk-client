232026be2a1a45fca11bd7ae6dfd9b66b8ba7af8
Nicklaspoke <Nicklas.poke@gmail.com>
2020-09-04 15:15:04 +0200

    Base files added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'normalize.css';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>OwO Index Page</h1>
+        </div>
+    );
+}
+
+export default App;
