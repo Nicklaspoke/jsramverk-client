0e762400263a512d135fdeb6fe2edf575778f56d
Nicklaspoke <Nicklas.poke@gmail.com>
2020-09-07 14:57:16 +0200

    Added file for handling page transiton animations

---
+++ src/PageAnimations.js
@@ -0,0 +1,12 @@
+export const PageTransitions = {
+    in: {
+        opacity: 1,
+        x: 0,
+    },
+    out: {
+        opacity: 0,
+        x: '-100vh',
+    }
+}
+
+export default { PageTransitions }
