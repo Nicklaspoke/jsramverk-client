2adcd77b7d34262860ae273d556f81e07418d03a
Nicklaspoke <Nicklas.poke@gmail.com>
2020-09-08 01:29:28 +0200

    Added some style changes for reports

--- src/ReusableStyles.js
+++ src/ReusableStyles.js
@@ -2,7 +2,7 @@
  * File for exportng different reusable jsx style rules
  */
 export const ReportStyle = {
-    padding: '1rem',
+    margin: '0.5rem 1.5rem 0 1.5rem',
     position: 'absolute'
 }
 
