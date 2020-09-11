45da936542d6b6f911c75af2b0dadd7312a37d8e
Nicklaspoke <Nicklas.poke@gmail.com>
2020-09-11 04:13:10 +0200

    Changed so kmom01 fetches data from the API

--- src/components/pages/reports/Kmom01.js
+++ src/components/pages/reports/Kmom01.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import marked from "marked";
 import { Layout } from 'antd';
 import { motion } from 'framer-motion';
 import { SlideInRight } from '../../../PageAnimations';
 import { ReportStyle } from '../../../ReusableStyles';
 import { GithubOutlined } from '@ant-design/icons';
+import ReactMarkdown from 'react-markdown';
 
 const { Content } = Layout;
 const { variations, transition } = SlideInRight
 
+const content = {}
 export default function Kmom01() {
+    const [state, setState] = useState({});
+
+    useEffect(() => {
+        retriveData();
+    }, [])
+
+    const retriveData = async () => {
+        let res = await fetch('http://localhost:8080/reports/week/1');
+        res = await res.json();
+        setState(res.data);
+    }
+
     return (
         <motion.div initial='initial' animate='in' exit='out' variants={variations} transition={transition}>
             <Content style={ReportStyle}>
-                <h1>Kmom01</h1>
+                <h1>{state.titel}</h1>
                 <GithubOutlined />
                 <a
                     href='https://github.com/Nicklaspoke/jsramverk-client'
@@ -22,17 +37,8 @@ export default function Kmom01() {
                 >
                     Github
                 </a>
-                <h1>Content of README</h1>
-                <h1>jsramverk-me-page</h1>
-                <h2> How to run the application</h2>
-                <h3> prerequisites</h3>
-                <ul>
-                    <li>nodejs version >= 12 (developed on version 14, but should work with the latest lts)</li>
-                </ul>
-                <p>
-                    The easiest way to get the application up and running is to first use "npm i" to install all dependencies, after that use "npm start" to begin runnint the development server.
-                    Once the server has started, it can be accessed on localhost:3000
-                </p>
+                <ReactMarkdown source={state.content} />
+
             </Content>
         </motion.div>
     );
