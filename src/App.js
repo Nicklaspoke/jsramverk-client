5cd893acb3e2d3981f06068acbde79cec759d49e
Nicklaspoke <Nicklas.poke@gmail.com>
2020-09-14 00:13:39 +0200

    Added global app context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion';
 import { Layout } from 'antd';
 import 'normalize.css';
 import './App.css';
 
 //Components Imports
+import AppContext from './AppContext';
 import Header from './components/layout/Header';
 import Sidebar from './components/layout/Sidebar';
 import Footer from './components/layout/Footer';
-import Index from './components/pages/Index'
+import Index from './components/pages/Index';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
 import Kmom01 from './components/pages/reports/Kmom01';
 import Kmom02 from './components/pages/reports/Kmom02';
 import Kmom03 from './components/pages/reports/Kmom03';
@@ -20,30 +23,43 @@ import Kmom10 from './components/pages/reports/Kmom10';
 
 function App() {
     const location = useLocation();
+
+    // Global state variables and functions
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const userSettings = {
+        isLoggedIn: isLoggedIn,
+        setIsLoggedIn,
+    };
+
     return (
-        <div className="App">
-            <Layout>
-                <Header />
+        <AppContext.Provider value={userSettings}>
+            <div className="App">
                 <Layout>
-                    <Sidebar />
-                    <Layout style={{ minHeight: '80vh', position: 'relative'}}>
-                        <AnimatePresence>
-                            <Switch location={location} key={location.pathname}>
-                                <Route exact path='/' component={Index} />
-                                <Route path='/reports/week/1' component={Kmom01} />
-                                <Route path='/reports/week/2' component={Kmom02} />
-                                <Route path='/reports/week/3' component={Kmom03} />
-                                <Route path='/reports/week/4' component={Kmom04} />
-                                <Route path='/reports/week/5' component={Kmom05} />
-                                <Route path='/reports/week/6' component={Kmom06} />
-                                <Route path='/reports/week/10' component={Kmom10} />
-                            </Switch>
-                        </AnimatePresence>
+                    <Header />
+                    <Layout>
+                        <Sidebar />
+                        <Layout style={{ minHeight: '80vh', position: 'relative' }}>
+                            <AnimatePresence>
+                                <Switch location={location} key={location.pathname}>
+                                    <Route exact path="/" component={Index} />
+                                    <Route path="/reports/week/1" component={Kmom01} />
+                                    <Route path="/reports/week/2" component={Kmom02} />
+                                    <Route path="/reports/week/3" component={Kmom03} />
+                                    <Route path="/reports/week/4" component={Kmom04} />
+                                    <Route path="/reports/week/5" component={Kmom05} />
+                                    <Route path="/reports/week/6" component={Kmom06} />
+                                    <Route path="/reports/week/7" component={Kmom10} />
+                                    <Route path="/auth/login" component={Login} />
+                                    <Route path="/auth/register" component={Register} />
+                                </Switch>
+                            </AnimatePresence>
+                        </Layout>
                     </Layout>
+                    <Footer />
                 </Layout>
-                <Footer />
-            </Layout>
-        </div>
+            </div>
+        </AppContext.Provider>
     );
 }
 
---
+++ src/AppContext.js
@@ -0,0 +1,8 @@
+/**
+ * Component for stroring App level wide states, such as login status
+ */
+import React from 'react';
+
+const AppContext = React.createContext();
+
+export default AppContext;
